d6fdf0d7bebb18bd6d593cf6cf9c23ddd45254ba
Rino Arias <rinoariaslaaz@gmail.com>
2024-07-19 14:25:29 -0500

    nuevo commit

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'tasksdb',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': '12345678',
         'HOST': 'localhost',
         'PORT': '3306',
     }
